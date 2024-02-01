ce1379d39210392b75f5da92e0e8f6635e349ebb
Daniel Tulpa <dtulpa16@yahoo.com>
2024-02-01 10:27:23 -0700

    Init

---
+++ src/index.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import ProductPage from './Pages/ProductsPage/ProductPage';
+import ProductDetailsPage from './Pages/ProductDetailsPage/ProductDetailsPage';
+import ShoppingCartPage from './Pages/ShoppingCartPage/ShoppingCartPage';
+import LoginPage from './Pages/LoginPage/LoginPage';
+import RegistrationPage from './Pages/RegistrationPage/RegistrationPage';
+
+const router = createBrowserRouter([
+  {
+    path: "",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <ProductPage />,
+      },
+      {
+        path: "/product/details/:productId",
+        element: <ProductDetailsPage />,
+      },
+      {
+        path: "/shoppingcart",
+        element: <ShoppingCartPage />,
+      },
+      {
+        path: "/login",
+        element: <LoginPage />,
+      },
+      {
+        path: "/register",
+        element: <RegistrationPage />,
+      },
+    ],
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
