d6c18d1b2da6d22158c0c75c69c0651f742a0599
Daniel Tulpa <dtulpa16@yahoo.com>
2024-02-02 11:09:17 -0700

    Shopping Cart Page

--- src/Pages/ShoppingCartPage/ShoppingCartPage.jsx
+++ src/Pages/ShoppingCartPage/ShoppingCartPage.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, { useState } from "react";
+import axios from "axios";
+import "../ProductsPage/ProductPage.css";
+import { useLoaderData } from "react-router-dom";
+import ProductList from "../../Components/ProductList";
+
+export async function getCart() {
+  let token = localStorage.getItem("token");
+  debugger
+  let response = await axios.get(`/shoppingcarts`, {
+    headers: { Authorization: "Bearer " + token },
+  });
+  const products = response.data.map((el) => el.product);
+  return products;
+}
 
 export default function ShoppingCartPage() {
+  const data = useLoaderData();
+  const [products] = useState(data);
   return (
-    <div>ShoppingCartPage</div>
-  )
+    <div>
+      <ProductList products={products} />
+    </div>
+  );
 }
--- src/index.js
+++ src/index.js
@@ -2,15 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import ProductPage, { getProducts } from "./Pages/ProductsPage/ProductPage";
-import ProductDetailsPage, { getProductDetails } from "./Pages/ProductDetailsPage/ProductDetailsPage";
-import ShoppingCartPage from "./Pages/ShoppingCartPage/ShoppingCartPage";
+import ProductDetailsPage, {
+  getProductDetails,
+} from "./Pages/ProductDetailsPage/ProductDetailsPage";
+import ShoppingCartPage, {
+  getCart,
+} from "./Pages/ShoppingCartPage/ShoppingCartPage";
 import LoginPage from "./Pages/LoginPage/LoginPage";
 import RegistrationPage from "./Pages/RegistrationPage/RegistrationPage";
 import { AuthProvider } from "./utils/useAuth";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import axios from 'axios';
-axios.defaults.baseURL = "http://localhost:5000/api"
+import axios from "axios";
+axios.defaults.baseURL = "http://localhost:5000/api";
 
 const router = createBrowserRouter([
   {
@@ -20,16 +24,17 @@ const router = createBrowserRouter([
       {
         path: "/",
         element: <ProductPage />,
-        loader:getProducts
+        loader: getProducts,
       },
       {
         path: "/product/details/:productId",
         element: <ProductDetailsPage />,
-        loader:getProductDetails
+        loader: getProductDetails,
       },
       {
         path: "/shoppingcart",
         element: <ShoppingCartPage />,
+        loader: getCart,
       },
       {
         path: "/login",
